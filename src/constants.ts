2b6aa716e7c594cf758097ed03cdad88955c2099
kangarooswapfinance <kangarooswapfinance@gmail.com>
2024-10-14 01:03:37 +0700

    sdk

---
+++ src/constants.ts
@@ -0,0 +1,77 @@
+import JSBI from 'jsbi'
+//import { bytecode } from '@eggswap/core/build/contracts/UniswapV2Pair.json'
+//import { keccak256 } from '@ethersproject/solidity'
+
+//const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`${bytecode}`])
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 1,
+  EXPANSE = 2,
+  ROPSTEN = 3,
+  RINKEBY = 4,
+  GÖRLI = 5,
+  KOVAN = 42,
+  MELDCHAIN = 333000333
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+//export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+
+export const FACTORY_ADDRESS = {
+  1: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
+  2: '0xaf69b94F4A1BBb09aD54DAaFca4629D915BD84E5',
+  3: '0xb709d6d184B51dC811cAa1B4841f8bdC700cF84b',
+  4: '0xb709d6d184B51dC811cAa1B4841f8bdC700cF84b',
+  5: '0xb709d6d184B51dC811cAa1B4841f8bdC700cF84b',
+  42: '0xb709d6d184B51dC811cAa1B4841f8bdC700cF84b',
+  333000333: '0xa3A00a5091a49885F22D6FcA862041095BD3b662'
+}
+
+//export const INIT_CODE_HASH = COMPUTED_INIT_CODE_HASH;
+//export const INIT_CODE_HASH = '0xc0c875d9c0f62295072f21084a19a575ff5ef887aa5ed8ce83ccfc708901071a'
+
+export const INIT_CODE_HASH = {
+  1: '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
+  2: '0xc0c875d9c0f62295072f21084a19a575ff5ef887aa5ed8ce83ccfc708901071a',
+  3: '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
+  4: '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
+  5: '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
+  42: '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
+  333000333: '0x2712321a603d3737f3c0fcd302768cf92ba6068eae63a47394f5c1bc0f0278f0'
+}
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
---
+++ src/entities/currency.ts
@@ -0,0 +1,51 @@
+import JSBI from 'jsbi'
+
+import { SolidityType, ChainId } from '../constants'
+import { validateSolidityTypeInstance } from '../utils'
+
+/**
+ * A currency is any fungible financial instrument on Ethereum, including Ether and all ERC20 tokens.
+ *
+ * The only instance of the base class `Currency` is Ether.
+ */
+export class Currency {
+  public readonly decimals: number
+  public readonly symbol?: string
+  public readonly name?: string
+
+  /**
+   * The only instance of the base class `Currency`.
+   */
+  public static readonly ETHER: Currency = new Currency(18, 'ETH', 'Ether')
+  public static readonly MELDCHAIN: Currency = new Currency(18, 'gMELD', 'Meld')
+
+  /**
+   * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ETHER`.
+   * @param decimals decimals of the currency
+   * @param symbol symbol of the currency
+   * @param name of the currency
+   */
+  protected constructor(decimals: number, symbol?: string, name?: string) {
+    validateSolidityTypeInstance(JSBI.BigInt(decimals), SolidityType.uint8)
+
+    this.decimals = decimals
+    this.symbol = symbol
+    this.name = name
+  }
+}
+
+const ETHER = Currency.ETHER
+const MELDCHAIN = Currency.MELDCHAIN
+
+
+export const BASE = {
+  [ChainId.MAINNET]: ETHER,
+  [ChainId.EXPANSE]: EXPANSE,
+  [ChainId.ROPSTEN]: ETHER,
+  [ChainId.RINKEBY]: ETHER,
+  [ChainId.GÖRLI]: ETHER,
+  [ChainId.KOVAN]: ETHER,
+  [ChainId.MELDCHAIN]: MELDCHAIN
+}
+
+export { ETHER, MELDCHAIN }
